9afa624d3d9cce503d747543c86a78337110e9b8
Diego Hernández <dieherram@gmail.com>
2024-09-07 01:01:32 -0400

    add password to pool

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,9 +1,10 @@
-import pkg from 'pg'
+import pg from 'pg'
 import 'dotenv/config'
-const { Pool } = pkg
+const { Pool } = pg
 
 export const pool = new Pool({
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
